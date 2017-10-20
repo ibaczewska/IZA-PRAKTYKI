aafa8f5dfc4cc48413a7325c62f285c7c3b75338
i.baczewska <izabela.baczewska@gmail.com>
2017-10-20 14:48:33 +0200

    delete p

--- src/js/pauseCarousel.js
+++ src/js/pauseCarousel.js
@@ -1,25 +1,19 @@
-/* $(function () {
-    $('#playButton').click(function () {
-        $('#carouselExampleIndicators').carousel('cycle');
-    });
-    $('#playButton').click(function () {
-        $('#carouselExampleIndicators').carousel('pause');
-    });
-});
-*/
+
 $('.carousel').carousel({
-  interval: 1000
+  interval: 3000
 });
 
-var i = 0;
-var indicators = $('#carouselExampleIndicators');
+//Function to switch on/off carousel when click on "See Our Work" button
+
+/* var indicators = $('#carouselExampleIndicators');
 $('#btn-onoff').on('click', function() {
 
-    if(i == 0) {
-        indicators.carousel('pause'); /* off carousel */
+    if(i == false) {
+        indicators.carousel('pause'); // off carousel
         i++;
     } else {
-        indicators.carousel('cycle'); /* on carousel */
+        indicators.carousel('cycle'); // on carousel
         i--;
     }
 });
+ */
